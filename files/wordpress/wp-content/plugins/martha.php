efeb1eaffb2daf66b51eeff3c37b43046c035507
Marco Ceppi <marco@ceppi.net>
2012-04-17 12:20:36 -0400

    Fixed Martha plugin

--- files/wordpress/wp-content/plugins/martha.php
+++ files/wordpress/wp-content/plugins/martha.php
@@ -7,7 +7,7 @@ Version:        0.5
 Author URI:     http://brandonholtsclaw.com/
 */
 
-$badwolf = str_split($_SERVER['HTTP_HOST'],":");
+$badwolf = explode(':', $_SERVER['HTTP_HOST']);
 
 $martha_home = preg_replace('!://[a-z0-9.-]*!', '://' . $badwolf[0], get_option('home'));
 
