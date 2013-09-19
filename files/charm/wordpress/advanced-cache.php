176685b27d225345ba0fe3e9c9091bf34dd0e091
Marco Ceppi <marco@ceppi.net>
2013-09-19 08:18:50 -0400

    Include -o only if there are options

--- files/charm/wordpress/advanced-cache.php
+++ files/charm/wordpress/advanced-cache.php
@@ -1,24 +1,28 @@
 <?php
-global $wp_ffpc_config ;
-$wp_ffpc_config['port']='11211';
-$wp_ffpc_config['host']='127.0.0.1';
-$wp_ffpc_config['expire']=300;
-$wp_ffpc_config['invalidation_method']='1';
-$wp_ffpc_config['prefix_meta']='meta-';
-$wp_ffpc_config['prefix_data']='data-';
-$wp_ffpc_config['charset']='utf-8';
-$wp_ffpc_config['pingback_status']='1';
-$wp_ffpc_config['debug']='1';
-$wp_ffpc_config['syslog']=0;
-$wp_ffpc_config['cache_type']='memcache';
-$wp_ffpc_config['cache_loggedin']=0;
-$wp_ffpc_config['nocache_home']=0;
-$wp_ffpc_config['nocache_feed']=0;
-$wp_ffpc_config['nocache_archive']=0;
-$wp_ffpc_config['nocache_single']=0;
-$wp_ffpc_config['nocache_page']=0;
-$wp_ffpc_config['apc_compress']=1;
-
-
-include_once ('/var/www/wp-content/plugins/wp-ffpc/wp-ffpc-common.php');
-include_once ('/var/www/wp-content/plugins/wp-ffpc/advanced-cache.php');
+$wp_ffpc_config = array (
+  'localhost' =>
+  array (
+    'hosts' => '127.0.0.1:11211',
+    'expire' => '300',
+    'invalidation_method' => '2',
+    'prefix_meta' => 'meta-',
+    'prefix_data' => 'data-',
+    'charset' => 'utf-8',
+    'log_info' => 0,
+    'log' => '0',
+    'cache_type' => 'memcached',
+    'cache_loggedin' => 0,
+    'nocache_home' => 0,
+    'nocache_feed' => 0,
+    'nocache_archive' => 0,
+    'nocache_single' => 0,
+    'nocache_page' => 0,
+    'sync_protocols' => 0,
+    'persistent' => 0,
+    'response_header' => 0,
+    'generate_time' => 0,
+    'version' => '1.1.1',
+  ),
+);
+include_once ('/var/www/wordpress/wp-content/plugins/wp-ffpc/wp-ffpc-backend.php');
+include_once ('/var/www/wordpress/wp-content/plugins/wp-ffpc/wp-ffpc-acache.php');
